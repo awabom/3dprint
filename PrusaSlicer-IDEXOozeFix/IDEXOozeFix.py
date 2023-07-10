fc2ba20c2c137ae5306792dccf8a8c00d92b5f2d
Åke Wallebom <ake.wallebom@gmail.com>
2023-07-10 22:32:12 +0200

    The first tool change now mimics the rest of the tool changes even more, by not doing the extra 'normal retract' before starting print. So the extra 'ooze fix' priming is now a bit later than before.

--- PrusaSlicer-IDEXOozeFix/IDEXOozeFix.py
+++ PrusaSlicer-IDEXOozeFix/IDEXOozeFix.py
@@ -17,10 +17,28 @@ gcodeFile = sys.argv[len(sys.argv)-1]
 with open(gcodeFile, "r") as f:
   inputLines = f.readlines()
   
-# Example: G1 E-5 F1200 ; CUSTOM RETRACT - UNDO T1: G1 E5 F2400
+# Example in start g-code from custom printer profile: G1 E-5 F1200 ; CUSTOM RETRACT - UNDO T1: G1 E5 F2400
+
+# Example of first tool change: 
+# M109 S245 T1 ; set temperature and wait for it to be reached
+# ; printing object shape id:0 copy 0
+# G1 E-1.5
+# G1 X158.109 Y134.448 F7200
+# G1 E1.5 F2400
+# ;TYPE:External perimeter
+
+# Goal:
+# M109 S245 T1 ; set temperature and wait for it to be reached
+# ; printing object shape id:0 copy 0
+# G1 X158.109 Y134.448 F7200
+# G1 E5 F2400 ; OozeFix
+# ;TYPE:External perimeter
+
 
 regex_undo = re.compile('.*CUSTOM RETRACT - UNDO (?P<tool>T[\d]+): (?P<undo>.*)') # Matches a retract that should be unretracted
 regex_m109 = re.compile('^M109 .*(?P<tool>T[\d]+).*') # Matches a heat-and-wait command
+regex_retract = re.compile('^G1 E-.*') # Matches a retract
+regex_unretract = re.compile('^G1 E[^\-].*') # Matches an unretract
 
 outputLines = []
 
@@ -28,23 +46,41 @@ undoDone = False
 undoTool = None
 undoCommand = None
 
+removeRetract = False
+replaceUnretract = False
+
 for lineNum in range(len(inputLines)):
   inputLine = inputLines[lineNum]
   outputLines.append(inputLine)
   
-  if not undoDone:
+  if undoDone:
+    continue;
+  
+  if undoTool is None:
     undoMatch = regex_undo.match(inputLine)
     if bool(undoMatch):
       undoTool = undoMatch.group('tool')
       undoCommand = undoMatch.group('undo')
       continue
-  
+  elif removeRetract:
+    matchRetract = regex_retract.match(inputLine)
+    if bool(matchRetract):
+      outputLines.pop() # Remove this line from outputLines
+      removeRetract = False
+      replaceUnretract = True
+  elif replaceUnretract:
+    matchUnretract = regex_unretract.match(inputLine)
+    if bool(matchUnretract):
+      outputLines.pop()
+      outputLines.append(undoCommand + '\n')
+      replaceUnretract = False
+      undoDone = True
+  else: # Check for heat-and-wait command
     m109Match = regex_m109.match(inputLine)
     if bool(m109Match):
       heatTool = m109Match.group('tool')
       if heatTool == undoTool:
-        outputLines.append(undoCommand + '\n')
-        undoDone = True
+        removeRetract = True
 
 # Output modified g-code to file
 with open(gcodeFile, "w") as f:
