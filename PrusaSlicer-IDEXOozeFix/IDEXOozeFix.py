51de16917a764b3aa6cb9f104e099f0d700d6dad
Åke Wallebom <ake.wallebom@gmail.com>
2023-07-25 00:00:39 +0200

    Bugfix: PrusaSlicer does not always retract after tool-change, so the oozefix was placed in the wrong spot. Fixed by never removing the unnecessary retract/unretract - just adding the extra 'oozefix' unretract.

--- PrusaSlicer-IDEXOozeFix/IDEXOozeFix.py
+++ PrusaSlicer-IDEXOozeFix/IDEXOozeFix.py
@@ -30,17 +30,17 @@ with open(gcodeFile, "r") as f:
 # Goal:
 # M109 S245 T1 ; set temperature and wait for it to be reached
 # ; printing object shape id:0 copy 0
+# G1 E-1.5
 # G1 X158.109 Y134.448 F7200
+# G1 E1.5 F2400
 # G1 E5 F2400 ; OozeFix
 # ;TYPE:External perimeter
 
 
-regex_undo = re.compile('.*CUSTOM RETRACT - UNDO (?P<tool>T[\d]+): (?P<undo>.*)') # Matches a retract that should be unretracted
+regex_undo = re.compile('.*CUSTOM RETRACT - UNDO (?P<tool>T[\d]+): (?P<undo>.*)') # Matches a retract that should be unretracted for a specific tool
 regex_extraprime = re.compile('.*EXTRAPRIME: (?P<prime>.*)') # Matches an 'extraprime' command to apply before first extrude
 regex_extrude = re.compile('^G1.*E[^\-].*') # Matches a G1 command that extrudes
 regex_m109 = re.compile('^M109 .*(?P<tool>T[\d]+).*') # Matches a heat-and-wait command
-regex_retract = re.compile('^G1 E-.*') # Matches a retract
-regex_unretract = re.compile('^G1 E[^\-].*') # Matches an unretract
 
 # 1st pass - Handle the first 'oozefix' retraction
 
@@ -50,42 +50,32 @@ undoDone = False
 undoTool = None
 undoCommand = None
 
-removeRetract = False
-replaceUnretract = False
+undoOnExtrude = False
 
 for lineNum in range(len(inputLines)):
   inputLine = inputLines[lineNum]
   outputLines.append(inputLine)
   
   if undoDone:
-    continue;
+    continue
   
   if undoTool is None:
     undoMatch = regex_undo.match(inputLine)
     if bool(undoMatch):
       undoTool = undoMatch.group('tool')
       undoCommand = undoMatch.group('undo')
-      continue
-  elif removeRetract:
-    matchRetract = regex_retract.match(inputLine)
-    if bool(matchRetract):
-      outputLines.pop() # Remove this line from outputLines
-      removeRetract = False
-      replaceUnretract = True
-  elif replaceUnretract:
-    matchUnretract = regex_unretract.match(inputLine)
-    if bool(matchUnretract):
-      outputLines.pop()
+  elif undoOnExtrude:
+    extrudeMatch = regex_extrude.match(inputLine)
+    if bool(extrudeMatch):
       outputLines.append(undoCommand + '\n')
-      replaceUnretract = False
       undoDone = True
   else: # Check for heat-and-wait command
     m109Match = regex_m109.match(inputLine)
     if bool(m109Match):
       heatTool = m109Match.group('tool')
       if heatTool == undoTool:
-        removeRetract = True
-        
+        undoOnExtrude = True
+
 
 # 2nd pass - Handle any 'EXTRAPRIME' entries
 inputLines = outputLines
