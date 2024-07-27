fcebadfd718d61fd000bec9b73329e6f911765f5
Åke Wallebom <ake.wallebom@gmail.com>
2024-07-27 11:41:46 +0200

    Prime tower required. Color change done in the middle of the prime tower sequence to avoid contaminating end of each filament print sequence

--- ColorSpliceList/ColorSpliceList.py
+++ ColorSpliceList/ColorSpliceList.py
@@ -26,17 +26,20 @@ cutFile += "-cutlist.txt"
 with open(gcodeFile, "r") as f:
   inputLines = f.readlines()
 
-currentCutSum = Decimal(0)
-currentCutList = 'unknown'
+currentSum = Decimal(0)
+currentPrintSum = Decimal(0)
+currentCutList = None
+nextCutList = None
 
-regex_cutlist = re.compile('^; MANUAL_TOOL_CHANGE T(?P<cut>.*)') # Cut list comment
+regex_change = re.compile('^; MANUAL_TOOL_CHANGE T(?P<cut>.*)') # Tool change
+regex_towerend = re.compile('^; CP TOOLCHANGE END') # Wipe tower end
 regex_disable = re.compile('^M600( .*)?') # Commands to comment out
 regex_extrusion = re.compile('^G1( .*)? E(?P<length>[^ ]*)') # G1 with extrusion
 
 outputLines = []
 cutLines = []
 
-# Go through file and count all extrusions between 'cut_list' comments, and comment out all 'disable' commands
+# Sum extrusions and produce a cut list
 for inputLine in inputLines:
   if bool(regex_disable.match(inputLine)): # Something to disable found - comment it out
     outputLines.append('; ' + inputLine)
@@ -44,18 +47,34 @@ for inputLine in inputLines:
     outputLines.append(inputLine)
     
     extMatch = regex_extrusion.match(inputLine)
-    cutMatch = regex_cutlist.match(inputLine)
-    if bool(extMatch):
+    changeMatch = regex_change.match(inputLine)
+    towerMatch = regex_towerend.match(inputLine)
+    
+    if bool(extMatch): # Is this an extrusion move? Just add it to sum
       extLength = Decimal(extMatch.group('length'))
-      currentCutSum += extLength
-    elif bool(cutMatch):
-      if currentCutSum > 0:
-        cutLines.append(currentCutList + "\t" + str(currentCutSum) + "\n")
-      currentCutList = cutMatch.group('cut')
-      currentCutSum = 0
+      currentSum += extLength
+    elif bool(changeMatch): # Found filament change
+      tool = changeMatch.group('cut')
+      if currentCutList is None: # First filament change command?
+        currentCutList = tool
+        if currentSum > 0: # There may be extrusions in the start g-code before manual filament change
+          cutLines.append("initial\t" + str(currentSum) + "\n")
+          currentSum = Decimal(0)
+      else:
+        nextCutList = tool
+        
+      # Store current "print moves sum" - used later when prime tower end is found
+      currentPrintSum = currentSum
+      currentSum = Decimal(0)
+    elif bool(towerMatch) and nextCutList != currentCutList: # found end of prime tower after a tool change, split the tower extrusions in half
+      split = currentSum / Decimal(2)
+      cutLines.append(currentCutList + "\t" + str(currentPrintSum + split) + "\n")
+      currentCutList = nextCutList
+      currentSum = split
+      currentPrintSum = Decimal(0)
 
-cutLines.append(currentCutList + "\t" + str(currentCutSum) + "\n")
-
+# Any extra extrusions left (no prime tower at end of print to detect)
+cutLines.append(currentCutList + "\t" + str(currentPrintSum + currentSum) + "\n")
 
 # Output modified g-code to file
 with open(gcodeFile, "w") as f:
